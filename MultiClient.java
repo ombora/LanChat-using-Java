b857f8f1d01db5272818c3640f0562a91f450c82
OMKAR BORATE <107358702+ombora@users.noreply.github.com>
2024-06-05 16:38:39 +0530

    Add files via upload

---
+++ MultiClient.java
@@ -0,0 +1,30 @@
+package chatapplication;
+
+import static chatapplication.MulticastClient.name;
+import javax.swing.JOptionPane;
+
+public class MultiClient {
+    MultiClient() { name= JOptionPane.showInputDialog("Please enter your name");
+        int count=0;
+      while( name==null || name.equals("") )
+       { if(name==null)
+       {   
+       new ChatApp().setVisible(true);
+      count++;
+       break;}
+       else if(name.equals(""))
+       {   JOptionPane.showMessageDialog(new ChatApp(), "Please enter a proper name");
+                   name=JOptionPane.showInputDialog("Please enter your name");
+       }
+   
+       }
+       if(count==0)
+       { new MulticastClient().setVisible(true);
+       Thread t1=new Thread(new Client());
+        t1.start();
+         
+            
+         
+       }
+}
+}
